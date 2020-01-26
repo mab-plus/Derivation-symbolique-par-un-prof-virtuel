cbf8df8cdebe72698604f17d523c8f36d12a8a9f
Michel BAKHTAOUI <michel.bakhtaoui@etu.univ-amu.fr>
2020-01-26 13:51:31 +0100

    Ajout commentaires pour Javadoc

--- src/edu/projet/professeur/Main.java
+++ src/edu/projet/professeur/Main.java
@@ -1,21 +1,63 @@
 package edu.projet.professeur;
 
-import java.util.Scanner;
-
-import edu.projet.calcul.*;
-import edu.projet.expressions.*;
-import edu.projet.interfaces.*;
-
+/**
+ * Graphics is the abstract base class for all graphics contexts
+ * which allow an application to draw onto components realized on
+ * various devices or onto off-screen images.
+ * A Graphics object encapsulates the state information needed
+ * for the various rendering operations that Java supports.  This
+ * state information includes:
+ * <ul>
+ * <li>The Component to draw on
+ * <li>A translation origin for rendering and clipping coordinates
+ * <li>The current clip
+ * <li>The current color
+ * <li>The current font
+ * <li>The current logical pixel operation function (XOR or Paint)
+ * <li>The current XOR alternation color
+ *     (see <a href="#setXORMode">setXORMode</a>)
+ * </ul>
+ * <p>
+ * Coordinates are infinitely thin and lie between the pixels of the
+ * output device.
+ * Operations which draw the outline of a figure operate by traversing
+ * along the infinitely thin path with a pixel-sized pen that hangs
+ * down and to the right of the anchor point on the path.
+ * Operations which fill a figure operate by filling the interior
+ * of the infinitely thin path.
+ * Operations which render horizontal text render the ascending
+ * portion of the characters entirely above the baseline coordinate.
+ * <p>
+ * Some important points to consider are that drawing a figure that
+ * covers a given rectangle will occupy one extra row of pixels on
+ * the right and bottom edges compared to filling a figure that is
+ * bounded by that same rectangle.
+ * Also, drawing a horizontal line along the same y coordinate as
+ * the baseline of a line of text will draw the line entirely below
+ * the text except for any descenders.
+ * Both of these properties are due to the pen hanging down and to
+ * the right from the path that it traverses.
+ * <p>
+ * All coordinates which appear as arguments to the methods of this
+ * Graphics object are considered relative to the translation origin
+ * of this Graphics object prior to the invocation of the method.
+ * All rendering operations modify only pixels which lie within the
+ * area bounded by both the current clip of the graphics context
+ * and the extents of the Component used to create the Graphics object.
+ * 
+ * @author      BAKHTAOUI Michel
+ * @version     1.0
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		
-		//test
-		//System.out.println("> Professeur : " + Professeur.reponse("je suis étudiant et j'ai besoin d'aide et je veux dériver sin(x)"));
+		/*test
+		System.out.println("> Professeur : " + Professeur.reponse("je suis étudiant et j'ai besoin d'aide et je veux dériver sin(x)"));
 		
 		System.out.println("> Professeur : " + Professeur.reponse("j'ai besoin de dériver cette fonction sin(1/x)"));
 		
-		/*String reponse, question;  
+		String reponse, question;  
 		Scanner sc = new Scanner(System.in);	 
 		System.out.println("> Professeur :" + Professeur.reponse("xdebut"));
 		while (sc.hasNextLine()) {
@@ -33,7 +75,7 @@ public class Main {
 		sc.close();*/
 		
 		
-		/*ProfBot bot=new ProfBot("Prof");
+		ProfBot bot=new ProfBot("Prof");
 		try {		
 			// Enable debugging output.
 	        bot.setVerbose(false);
@@ -46,7 +88,7 @@ public class Main {
 		}
 		catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 		
 	}
 
--- src/edu/projet/professeur/ProfBot.java
+++ src/edu/projet/professeur/ProfBot.java
@@ -2,12 +2,19 @@ package edu.projet.professeur;
 
 import org.jibble.pircbot.PircBot;
 
-
 /**
+ * ProfBot utilise l'API IRC Java
+ * PircBot est un framework Java pour écrire des bots IRC rapidement et facilement. 
+ * Ses fonctionnalités incluent une architecture événementielle pour gérer les événements IRC courants. 
+ * Son format de fichier journal complet peut être utilisé avec pisg pour générer des statistiques de canal. 
+ * Lorsque vous compilez ou exécutez votre propre Bot qui utilise le package pircbot, vous devez vous assurer d'inclure le fichier pircbot.jar dans votre chemin de classe.
+ * Comment faire un Bot IRC
+ * Le package pircbot contient (entre autres) une classe abstraite nommée PircBot. Parce que c'est abstrait, cela signifie que vous ne pouvez pas en créer une instance, 
+ * mais vous pouvez l'étendre et hériter de ses fonctionnalités. 
  * @author BAKHTAOUI Michel
  * @version 1.0
  * @see PircBot, http://www.jibble.org/pircbot.php
- */
+*/
 public class ProfBot extends PircBot {
 	
 	public ProfBot(String name){
